1b62a55371708ae0835fbe4e6414edfc4eb38c0f
Benny <mail2benny+github@gmail.com>
2023-04-24 09:24:58 +0200

    Refactor

--- src/Taglit.jl
+++ src/Taglit.jl
@@ -2,69 +2,86 @@ module Taglit
 
 export Tagmap, references
 
-struct Object{T}
+struct Object{T, U}
     data::T
-    references::Set{String}
+    references::Set{U}
 end
-Object(value::T) where T = Object(value, Set{String}())
+Object(value::T, ::Type{U} = String) where {T, U} = Object(value, Set{U}())
 data(object::Object) = object.data
 references(object::Object) = object.references
 key(object::Object) = hash(object.data)
 
-struct Tagmap
-    references::Dict{String, Set{UInt}}
-    objects::Dict{UInt, Object}
+function Base.show(io::IO, ::MIME"text/plain", object::Object)
+    show(io, "text/plain", data(object))
+    print(io, " <= $(collect(references(object)))")
 end
-Tagmap() = Tagmap(Dict{String, Set{UInt}}(), Dict{UInt, Object}())
-references(container::Tagmap) = container.references
-references(container::Tagmap, value::T) where T = references(objects(container)[key(Object(value))])
-objects(container::Tagmap) = container.objects
-Base.keys(container::Tagmap) = keys(objects(container))
-Base.values(container::Tagmap) = values(objects(container))
-Base.length(container::Tagmap) = length(references(container))
 
-function Base.push!(container::Tagmap, value::Object)
+struct Tagmap{T, U}
+    references::Dict{U, Set{UInt}}
+    objects::Dict{UInt, Object{T, U}}
+end
+Tagmap(::Type{T} = String, ::Type{U} = String) where {T, U} = Tagmap(Dict{U, Set{UInt}}(), Dict{UInt, Object{T, U}}())
+objecttype(_::Tagmap{T, U}) where {T, U} = T
+referencetype(_::Tagmap{T, U}) where {T, U} = U
+references(tagmap::Tagmap) = tagmap.references
+references(tagmap::Tagmap, value) = references(objects(tagmap)[key(Object(value))])
+objects(tagmap::Tagmap) = tagmap.objects
+Base.keys(tagmap::Tagmap) = keys(objects(tagmap))
+Base.values(tagmap::Tagmap) = data.(values(objects(tagmap)))
+Base.length(tagmap::Tagmap) = length(objects(tagmap))
+
+function Base.show(io::IO, ::MIME"text/plain", tagmap::Tagmap{T, U}) where {T, U}
+    print(io, "$(length(tagmap))-element Tagmap{$T, $U}:")
+    for value in collect(values(objects(tagmap)))
+        print(io, "\n ")
+        show(io, "text/plain", value)
+    end
+end
+
+function Base.push!(tagmap::Tagmap, value::Object)
     k = key(value)
-    if haskey(objects(container), k)
-        for reference in setdiff(references(value), references(objects(container)[k]))
-            if haskey(container.references, reference)
-                push!(container.references[reference], k)
+    if haskey(objects(tagmap), k)
+        for reference in setdiff(references(value), references(objects(tagmap)[k]))
+            if haskey(tagmap.references, reference)
+                push!(tagmap.references[reference], k)
             else
-                push!(container.references, reference => Set(k))
+                push!(tagmap.references, reference => Set(k))
             end
         end
-        for reference in setdiff(references(objects(container)[k]), references(value))
-            pop!(container.references[reference], k)
-            if isempty(container.references[reference])
-                pop!(container.references, reference)
+        for reference in setdiff(references(objects(tagmap)[k]), references(value))
+            pop!(tagmap.references[reference], k)
+            if isempty(tagmap.references[reference])
+                pop!(tagmap.references, reference)
             end
         end
         if isempty(references(value))
-            pop!(container.objects, k)
+            pop!(tagmap.objects, k)
         else
-            push!(objects(container), k => value)
+            push!(objects(tagmap), k => value)
         end
     else
-        push!(container.objects, k => value)
-        for reference in references(value)
-            if haskey(container.references, reference)
-                push!(container.references[reference], k)
-            else
-                push!(container.references, reference => Set(k))
+        if !isempty(references(value))
+            push!(tagmap.objects, k => value)
+            for reference in references(value)
+                if haskey(tagmap.references, reference)
+                    push!(tagmap.references[reference], k)
+                else
+                    push!(tagmap.references, reference => Set(k))
+                end
             end
         end
     end
-    return container
+    return tagmap
 end
-Base.push!(container::Tagmap, value::T, args::Set{String}) where T = push!(container, Object(value, args))
-Base.push!(container::Tagmap, value::T, args::String...) where T = push!(container, Object(value, Set([args...])))
-Base.push!(container::Tagmap, value::T, args::Vector{String}) where T = push!(container, Object(value, Set(args)))
+Base.push!(tagmap::Tagmap, value::T, args::Set{U}) where {T, U} = push!(tagmap, Object{T, U}(value, args))
+Base.push!(tagmap::Tagmap, value::T, args::U...) where {T, U} = push!(tagmap, Object(value, Set{U}([args...])))
+Base.push!(tagmap::Tagmap, value::T, args::Vector{U} = referencetype(tagmap)[]) where {T, U} = push!(tagmap, Object(value, Set(args)))
 
-function Base.getindex(container::Tagmap, args::Set{String})
-    targets = [references(container)[arg] for arg in args]
-    return isempty(targets) ? nothing : data.([objects(container)[target] for target in intersect(targets...)])
+function Base.getindex(tagmap::Tagmap, args::Set{U}) where U
+    targets = [references(tagmap)[arg] for arg in args]
+    return isempty(targets) ? empty(values[tagmap]) : data.([objects(tagmap)[target] for target in intersect(targets...)])
 end
-Base.getindex(container::Tagmap, args::Vector{String}) = getindex(container, Set(args))
-Base.getindex(container::Tagmap, args::String...) = getindex(container, Set([args...]))
+Base.getindex(tagmap::Tagmap, args::Vector{U} = referencetype(tagmap)[]) where U = getindex(tagmap, Set{U}(args))
+Base.getindex(tagmap::Tagmap, args::U...) where U = getindex(tagmap, Set{U}([args...]))
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,13 @@
 #!/usr/bin/env julia
 using Taglit
 using Test
+using Random
 
 @testset "One Value" begin
     tagmap = Tagmap()
     @test length(tagmap) == 0
     push!(tagmap, "User 1", "Programmer", "Father", "Smart")
-    @test length(tagmap) == 3
+    @test length(references(tagmap)) == 3
     @test references(tagmap, "User 1") == Set(["Programmer", "Father", "Smart"])
     @test tagmap["Programmer"] == ["User 1"]
     @test tagmap[["Programmer"]] == ["User 1"]
@@ -14,10 +15,10 @@ using Test
     @test tagmap["Programmer", "Father"] == ["User 1"]
     @test tagmap["Programmer", "Father"] == tagmap["Programmer", "Father"]
     push!(tagmap, "User 1", ["Programmer", "Father", "Smart", "Nerd"])
-    @test length(tagmap) == 4
+    @test length(references(tagmap)) == 4
     @test references(tagmap, "User 1") == Set(["Programmer", "Father", "Smart", "Nerd"])
     push!(tagmap, "User 1", Set(["Programmer", "Father", "Smart"]))
-    @test length(tagmap) == 3
+    @test length(references(tagmap)) == 3
     @test references(tagmap, "User 1") == Set(["Programmer", "Father", "Smart"])
 end
 
@@ -26,7 +27,7 @@ end
     @test length(tagmap) == 0
     push!(tagmap, "User 1", "Programmer", "Father", "Smart")
     push!(tagmap, "User 2", "Programmer", "Single", "Dumb")
-    @test length(tagmap) == 5
+    @test length(references(tagmap)) == 5
     @test references(tagmap, "User 1") == Set(["Programmer", "Father", "Smart"])
     @test references(tagmap, "User 2") == Set(["Programmer", "Single", "Dumb"])
     @test length(tagmap["Programmer"]) == 2
@@ -34,4 +35,63 @@ end
     push!(tagmap, "User 2", "Programmer", "Father", "Dumb")
     @test references(tagmap, "User 2") == Set(["Programmer", "Father", "Dumb"])
     @test length(tagmap["Father"]) == 2
+end
+
+@testset "Two Value Symbols" begin
+    tagmap = Tagmap(Symbol, Symbol)
+    @test length(tagmap) == 0
+    push!(tagmap, :User1, :Programmer, :Father, :Smart)
+    push!(tagmap, :User2, :Programmer, :Single, :Dumb)
+    @test length(references(tagmap)) == 5
+    @test references(tagmap, :User1) == Set([:Programmer, :Father, :Smart])
+    @test references(tagmap, :User2) == Set([:Programmer, :Single, :Dumb])
+    @test length(tagmap[:Programmer]) == 2
+    @test length(tagmap[:Father]) == 1
+    push!(tagmap, :User2, :Programmer, :Father, :Dumb)
+    @test references(tagmap, :User2) == Set([:Programmer, :Father, :Dumb])
+    @test length(tagmap[:Father]) == 2
+end
+
+@testset "Clear" begin
+    tagmap = Tagmap()
+    @test length(references(tagmap)) == 0
+    @test length(tagmap) == 0
+    push!(tagmap, "User 1", "Programmer", "Father", "Smart")
+    push!(tagmap, "User 2", "Programmer", "Single", "Dumb")
+    @test length(references(tagmap)) == 5
+    @test Set(values(tagmap)) == Set(["User 1", "User 2"])
+    push!(tagmap, "User 1")
+    @test length(references(tagmap)) == 3
+    @test Set(values(tagmap)) == Set(["User 2"])
+    push!(tagmap, "User 2")
+    @test length(references(tagmap)) == 0
+    @test length(tagmap) == 0
+    push!(tagmap, "User 1")
+    push!(tagmap, "User 2")
+    @test length(references(tagmap)) == 0
+    @test length(tagmap) == 0
+end
+
+@testset "Stress" begin
+    N = 2^16
+    M = 2^4
+    K = 2^6
+    L = 2^10
+    rg = Random.seed!(57)
+    objects = [randstring(rg) for _ in 1:N]
+    utags = [randstring(rg) for _ in 1:K]
+    tagmap = Tagmap()
+    @test length(tagmap) == 0
+    for object in objects
+        push!(tagmap, object, rand(rg, utags, rand(rg, 1:M)))
+    end
+    @test length(tagmap) == N
+    for lookup in rand(rg, reduce(vcat, collect(Iterators.product(utags, utags))), L)
+        @test tagmap[lookup...] == tagmap[Set([lookup...])]
+    end
+    @test length(tagmap) == N
+    for object in objects
+        push!(tagmap, object)
+    end
+    @test length(tagmap) == 0
 end
